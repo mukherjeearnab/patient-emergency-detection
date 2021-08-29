6c071b9ed9f1e482218d5f786cd2e2b2f670b0ef
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2021-08-29 12:31:29 +0530

    Added Detection Route

---
+++ middleware/src/routes/detection.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const JWTmiddleware = require("../helpers/jwtVerifyMiddleware");
+const DetectionContract = require("../../fabric/contracts/detection");
+
+const router = new express.Router();
+
+router.get("/api/detection/check/:id", JWTmiddleware, async (req, res) => {
+    try {
+        let data = DetectionContract.CheckReading(
+            { username: req.user.username, organization: req.user.organization },
+            [req.params.id, req.body.data.Cipher]
+        );
+        res.status(200).send({
+            detection: data,
+        });
+    } catch (error) {
+        res.status(404).send({ message: "Asset NOT found!" });
+    }
+});
+
+module.exports = router;
