bd77f5dd902887d37f67fc42624d03d760e10c57
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2021-09-06 16:07:45 +0530

    Updated Middleware setup

--- middleware/setup/set_tpe_config.js
+++ middleware/setup/set_tpe_config.js
@@ -1,8 +1,13 @@
+require("dotenv").config();
+
 const TPEContract = require("../fabric/contracts/tpe");
 
 const main = async () => {
     try {
-        await TPEContract.SetConfig({ username: "admin", organization: "phc" }, [5, 28.1]);
+        await TPEContract.SetConfig({ username: "admin", organization: "phc" }, [
+            process.env.TPE_N,
+            process.env.TPE_THETA,
+        ]);
     } catch (error) {
         console.error("TPE Config set!", error);
     }
