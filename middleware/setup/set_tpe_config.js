82e60ec28e7c58344f91e9b6e2bc2b297fbda49a
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2021-09-06 16:05:59 +0530

    Resolved Typo in Middleware setup

--- middleware/setup/set_tpe_config.js
+++ middleware/setup/set_tpe_config.js
@@ -2,7 +2,7 @@ const TPEContract = require("../fabric/contracts/tpe");
 
 const main = async () => {
     try {
-        await TPEContract.SetConfig({ username: "Admin", organization: "phc" }, [5, 28.1]);
+        await TPEContract.SetConfig({ username: "admin", organization: "phc" }, [5, 28.1]);
     } catch (error) {
         console.error("TPE Config set!", error);
     }
